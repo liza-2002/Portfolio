02915c867e6c816778a7310070d53eee1b91d1d8
Liza Rani Biswal <lizaranibiswal97@gmail.com>
2023-09-04 15:07:59 +0530

    update homepage

---
+++ personal-portfolio/src/Components/Intro/Intro.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Intro.css';
+import Github from '../../img/github.png';
+import LinkedIn from '../../img/linkedin.png';
+import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import girl from '../../img/girl.png';
+import thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+function Intro() {
+    return (
+        <div className="intro">
+            <div className="i-left">
+                <div className="i-name">
+                    <span>
+                        Hy! I Am
+                    </span>
+                    <span>
+                        Liza
+                    </span>
+                    <span>
+                        Highly motivated student with
+                         a passion for technology and a 
+                         strong desire to learn in the <br/>field 
+                         of Information Technology.
+                        Quick learner and adaptable team player, 
+                        eager to collaborate <br/>with colleagues to 
+                        achieve project goals.
+                    </span>
+                </div>
+                <button className="button i-button">Hire me</button>
+                <div className="i-icons">
+                    <img src={Github} alt="" />
+                    <img src={LinkedIn} alt="" />
+                    <img src={Instagram} alt="" />
+                </div>
+            </div>
+            <div className="i-right">
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={girl} alt="" />
+                <img src={glassesimoji} alt="" />
+                <div style={{top:'-4%',left:'68%'}}>
+                    <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+                </div>
+                <div style={{top:'18rem',left:'0rem'}}>
+                    <FloatingDiv image={thumbup} txt1='Best' txt2='Design'/> 
+                </div>
+                {/*blur div*/}
+                <div className='blur' style={{background:"rgb(338 210 255"}}></div>
+                <div className='blur' style={{background:'#C1F5FF',top:'14rem',width:'21rem',height:'11rem',left:'-9%'}}></div>
+            </div>
+        </div>
+    )
+}
+
+export default Intro
